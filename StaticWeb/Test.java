7471f9ebd2c0a8895560d1944a060fba6e939be1
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-10-18 10:06:21 +0530

    ts

---
+++ StaticWeb/Test.java
@@ -0,0 +1,10 @@
+class Test{
+    public static void main(String[] args) {
+        Integer i = Integer.valueOf(10);
+        //Integer i = 10;
+        System.out.println(i.floatValue());
+        Integer x = Integer.parseInt("1");
+        System.out.println(x);
+        int b= i.intValue();
+    }
+}
